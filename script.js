8b9bf25e8f7e29e613481e476a4e1e11d3c14993
dinintan <fdini882@gmail.com>
2026-08-27 23:55:07 +0700

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-const tombol = document.getElementById("profilButton");
-const profil = document.getElementById("profil");
-
-tombol.addEventListener("click", function() {
-    profil.style.display = "block";
-});
